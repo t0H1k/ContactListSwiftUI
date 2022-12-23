1b7d11cb91f1725d834762d48e583b905f096015
Anton Boev <110462969+t0H1k@users.noreply.github.com>
2022-12-23 22:03:44 +0300

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Anton Boev on 23.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
